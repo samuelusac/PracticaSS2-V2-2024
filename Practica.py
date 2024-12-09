b122f4b5003b827ddf2a52567826f159f07538c5
Jaime Garcia <2220723340101@ingenieria.usac.edu.gt>
2024-12-09 16:14:46 +0000

    Extraccion

--- Practica.py
+++ Practica.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import io
+import requests
 
-df = pd.read_csv('index.csv')
+## Extraccion
 
-print(df.shape)
+## Archivo local
+
+df = pd.read_csv('municipio.csv')
+print("Se leyo el archivo municipio.csv, almacenado localmente")
+print (df.shape)
+## Archivo remoto
+
+url = "https://ay-disco.s3.us-east-2.amazonaws.com/global_calificacion.csv"
+s = requests.get(url).content
+df2 = pd.read_csv(io.StringIO(s.decode('utf-8')))
+print("Se leyo el archivo global_calificacion.csv, almacenado remotamente")
+print(df2.shape)
+
